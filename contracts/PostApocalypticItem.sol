077058c1d43d6d2215a8cc2f00006b760b1bb25b
piaocreative <johnpiao171019@gmail.com>
2021-12-01 00:46:55 +0800

    Updated contract

--- contracts/PostApocalypticItem.sol
+++ contracts/PostApocalypticItem.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PostApocalypticItem is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     // set contract name and ticker. 
-    constructor() ERC721("Post-Apocalyptic", "OSPA") {}
+    constructor() ERC721("Post-Apocalyptic", "PA") {}
 
     //get the current supply of tokens
     function totalSupply() public view returns (uint256) {
